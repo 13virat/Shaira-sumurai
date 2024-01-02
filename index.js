95fb19b644a7ec3e2fcef352071a65fc69a60461
13virat <viratgupta1314@gmail.com>
2024-01-02 14:12:57 +0530

    collision block

--- index.js
+++ index.js
@@ -1,123 +1,131 @@
-const canvas= document.querySelector('canvas')
-const c=canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width=1024
-canvas.height=576
+canvas.width = 1024;
+canvas.height = 576;
 
-const scaledCanvas={
-    width:canvas.width/4,
-    height:canvas.height/4,
+const scaledCanvas = {
+  width: canvas.width / 4,
+  height: canvas.height / 4,
+};
+
+const floorCollisions2D = [];
+for (let i = 0; i < floorCollisions.length; i += 36) {
+  floorCollisions2D.push(floorCollisions.slice(i, i + 36));
 }
 
-const gravity=.5
+const collisionBlocks = [];
 
-class Sprite{
-    constructor({position,imageSrc}){
-        this.position=position
-        this.image= new Image()
-        this.image.src=imageSrc
+floorCollisions2D.forEach((row, y) => {
+  row.forEach((symbol, x) => {
+    if (symbol === 192) {
+      collisionBlocks.push(
+        new CollisionBlock({
+          position: {
+            x: x * 16,
+            y: y * 16,
+          },
+        })
+      );
     }
-    draw(){
-        if(!this.image)return
-        c.drawImage(this.image,this.position.x,this.position.y)
-    }
-    update(){
-        this.draw()
-    }
+  });
+});
+const platformCollisions2D = [];
+for (let i = 0; i < platformCollisions.length; i += 36) {
+  platformCollisions2D.push(platformCollisions.slice(i, i + 36));
 }
 
-class Player{
-    constructor(position ){
-        this.position=position
-        this.velocity={
-            x:0,
-            y:1  
-        }
-        this.height=100
-    }
-    draw(){
-        c.fillStyle='red'
-        c.fillRect(this.position.x,this.position.y,100,this.height)
-    }
-    update(){
-        this.draw() 
+const platformCollisionBlocks = [];
 
-        this.position.x+=this.velocity.x
-        this.position.y+=this.velocity.y
-        
-        if (this.position.y+ this.height +this.velocity.y  < canvas.height) 
-            this.velocity.y+=gravity 
-        else this.velocity.y=0
+platformCollisions2D.forEach((row, y) => {
+  row.forEach((symbol, x) => {
+    if (symbol === 192) {
+      platformCollisionBlocks.push(
+        new CollisionBlock({
+          position: {
+            x: x * 16,
+            y: y * 16,
+          },
+        })
+      );
     }
+  });
+});
+console.log(platformCollisions2D);
+
+const gravity = 0.5;
+
+const player = new Player({
+  x: 0,
+  y: 0,
+});
+const player2 = new Player({
+  x: 300,
+  y: 100,
+});
+
+const keys = {
+  d: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+};
+
+const background = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imageSrc: "./img/background.png",
+});
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  c.save();
+  c.scale(4, 4);
+  c.translate(0, -background.image.height + scaledCanvas.height);
+  background.update();
+  collisionBlocks.forEach((collisionBlock) => {
+    collisionBlock.update();
+  });
+  platformCollisionBlocks.forEach((block) => {
+    block.update();
+  });
+  c.restore();
+
+  player.update();
+  player2.update();
+
+  player.velocity.x = 0;
+  if (keys.d.pressed) player.velocity.x = 5;
+  else if (keys.a.pressed) player.velocity.x = -5;
 }
-const player=new Player({
-    x:0,
-    y:0,
-})
-const player2=new Player({
-    x:300,
-    y:100,
-})
-
-const keys={
-    d:{
-        pressed:false,
-    },
-    a:{
-        pressed:false,
-    },
-    
-}
-
-const background= new Sprite({
-    position:{
-        x:0,
-        y:0,
-    },
-    imageSrc:'./img/background.png'
-})
-
-
-function animate(){
-    window.requestAnimationFrame(animate)
-    c.fillStyle='white'
-    c.fillRect(0,0,canvas.width,canvas.height)
-
-    c.save()
-    c.scale(4,4)
-    c.translate(0,-background.image.height+scaledCanvas.height)
-    background.update()
-    c.restore()
-
-    player.update()
-    player2.update()
-
-    player.velocity.x=0
-    if (keys.d.pressed) player.velocity.x=5
-    else if (keys.a.pressed) player.velocity.x=-5
-}
-animate()
-window.addEventListener('keydown',(event)=>{
-    switch(event.key){
-        case'd':
-         keys.d.pressed= true
-         break
-        case'a':
-        keys.a.pressed= true
-         break
-        case'w':
-         player.velocity.y=-20
-         break
-    }
-})
-window.addEventListener('keyup',(event)=>{
-    switch(event.key){
-        case'd':
-         keys.d.pressed= false
-         break
-        case'a':
-        keys.a.pressed= false
-         break
-        
-    }
-})
+animate();
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "w":
+      player.velocity.y = -20;
+      break;
+  }
+});
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+  }
+});
