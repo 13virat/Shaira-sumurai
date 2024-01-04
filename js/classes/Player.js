f510f84e83c69b88fe69400dd7fe601d8b6f277c
13virat <viratgupta1314@gmail.com>
2024-01-04 16:54:26 +0530

    horizontal camera

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -34,6 +34,13 @@ class Player extends Sprite {
       image.src = this.animations[key].imageSrc
 
       this.animations[key].image = image
+    }this.camerabox = {
+      position: {
+        x: this.position.x,
+        y: this.position.y,
+      },
+      width: 200,
+      height: 80,
     }
   }
   switchSprite(key) {
@@ -43,9 +50,58 @@ class Player extends Sprite {
     this.frameBuffer = this.animations[key].frameBuffer
     this.frameRate = this.animations[key].frameRate
   }
+  updateCamerabox() {
+    this.camerabox = {
+      position: {
+        x: this.position.x - 50,
+        y: this.position.y,
+      },
+      width: 200,
+      height: 80,
+    }
+  }
+
+  checkForHorizontalCanvasCollision() {
+    if (
+      this.hitbox.position.x + this.hitbox.width + this.velocity.x >= 576 ||
+      this.hitbox.position.x + this.velocity.x <= 0
+    ) {
+      this.velocity.x = 0
+    }
+  }
+
+  shouldPanCameraToTheLeft({ canvas, camera }) {
+    const cameraboxRightSide = this.camerabox.position.x + this.camerabox.width
+    const scaledDownCanvasWidth = canvas.width / 4
+
+    if (cameraboxRightSide >= 576) return
+
+    if (
+      cameraboxRightSide >=
+      scaledDownCanvasWidth + Math.abs(camera.position.x)
+    ) {
+      camera.position.x -= this.velocity.x
+    }
+  }
+
+  shouldPanCameraToTheRight({ canvas, camera }) {
+    if (this.camerabox.position.x <= 0) return
+
+    if (this.camerabox.position.x <= Math.abs(camera.position.x)) {
+      camera.position.x -= this.velocity.x
+    }
+  }
   update() {
     this.updateFrames();
     this.updateHitbox();
+    this.updateCamerabox()
+    c.fillStyle = 'rgba(0, 0, 255, 0.2)'
+    c.fillRect(
+      this.camerabox.position.x,
+      this.camerabox.position.y,
+      this.camerabox.width,
+      this.camerabox.height
+    )
     // c.fillStyle = "rgba(0, 255, 0, 0.2)";
     // c.fillRect(this.position.x, this.position.y, this.width, this.height);
 
